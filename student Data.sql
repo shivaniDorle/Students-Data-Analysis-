f488c6644e9ebdabac2a34951c90265830fc41ac
Shivani Dorle <shivanidorle23@gmail.com>
2024-09-30 11:53:54 +0530

    first commit

--- student Data.sql
+++ student Data.sql
@@ -1,4 +1,23 @@
 create database students;
 use students;
 
-select* from studentperformancefactors;
+SELECT Access_to_Resources, AVG(Sleep_Hours) AS Average_Sleep_Hours
+FROM studentperformancefactors
+GROUP BY Access_to_Resources;
+
+SELECT Extracurricular_Activities, AVG(Previous_Scores) AS Average_Previous_Scores
+FROM studentperformancefactors
+GROUP BY Extracurricular_Activities;
+
+SELECT Sleep_Hours, AVG(Previous_Scores) AS Average_Previous_Scores
+FROM studentperformancefactors
+GROUP BY Sleep_Hours;
+
+SELECT Internet_Access, Motivation_Level, COUNT(*) AS Count
+FROM studentperformancefactors
+GROUP BY Internet_Access, Motivation_Level;
+
+
+
+
+select *from studentperformancefactors;
